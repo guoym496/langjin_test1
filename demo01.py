9b3df8447e4b3901e0954ea14159a55fc6b335cb
guoym496 <982622152@qq.com>
2020-06-30 01:44:36 +0800

    第一次提交

---
+++ demo01.py
@@ -0,0 +1,17 @@
+'''
+print("你好!")
+print(22222)
+print(True)
+
+a="张三"
+print(a)
+
+a =float(input("请输入a"))
+b =float(input("请输入b"))
+print("获取的内容是：",a+b)
+
+'''
+
+a =input("请输入a")
+b =input("请输入b")
+print("获取的内容是：",a+b,"长度为：",len(a+b))
